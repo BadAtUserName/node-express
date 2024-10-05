891d93b0879fc894eae58e4befcee2e7346f824f
Amanda Johnson <amanda.j86@gmail.com>
2024-10-05 12:28:40 -0700

    "campsite, promotion, partner, routes updated"

--- routes/campsiteRouter.js
+++ routes/campsiteRouter.js
@@ -22,4 +22,26 @@ campsiteRouter.route('/')//¯\_(ツ)_/¯ anyone else feel like it has this vibe?
   res.end('Deleting all campsites');
 });
 
-module.exports = campsiteRouter
+// New route for '/:campsiteId'
+campsiteRouter.route('/:campsiteId')
+.all((req, res, next) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  next();
+})
+.get((req, res) => {
+  res.end(`Will send details of the campsite with ID: ${req.params.campsiteId} to you`);
+})
+.post((req, res) => {
+  res.statusCode = 403;
+  res.end(`POST operation not supported on /campsites/${req.params.campsiteId}`);
+})
+.put((req, res) => {
+  res.end(`Updating the campsite with ID: ${req.params.campsiteId} 
+    with name: ${req.body.name} and description: ${req.body.description}`);
+})
+.delete((req, res) => {
+  res.end(`Deleting campsite with ID: ${req.params.campsiteId}`);
+});
+
+module.exports = campsiteRouter;
